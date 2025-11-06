cede7f4e6386e6c108803ca04b50b25aefb03f3c
Hugo Jiménez López <lopez3109@hotmail.com>
2025-11-06 18:59:03 +0000

    Products

--- models/staging/sql_server_dbo/stg_sql_server_dbo__products.sql
+++ models/staging/sql_server_dbo/stg_sql_server_dbo__products.sql
@@ -6,8 +6,14 @@
 
 WITH src_products AS (
     SELECT 
-        *
+        MD5(PRODUCT_ID) AS PRODUCT_ID,
+        COALESCE(PRICE,0) AS PRICE_DOLLAR,
+        NAME AS PRODUCT_NAME,
+        INVENTORY AS INVENTORY_QUANTITY,
+        _FIVETRAN_DELETED AS _FIVETRAN_DELETED,
+        CONVERT_TIMEZONE('UTC',_FIVETRAN_SYNCED) AS DATA_UPDATED_UTC
     FROM {{ source('sql_server_dbo','products')}}
+    WHERE _FIVETRAN_DELETED IS NULL
 )
 
 SELECT * FROM src_products
